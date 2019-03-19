6e12f85ab87ca324baa46710885a2a1f475a48b9
Roman <42993647+romankoltun@users.noreply.github.com>
2019-03-19 12:23:25 +0200

    Add files via upload
    Teacher

---
+++ Scientific_report/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    
+        class Teacher
+        {
+            public string PIB { get; set; }
+            public string position { get; set; }
+            public string chair { get; set; }
+            public int year_of_birth { get; set; }
+            public int Year_of_graduation { get; set; }
+            public string Degree { get; set; }
+            public int year_of_protection { get; set; }
+            public string Academic_status { get; set; }
+            public int year_of_assignment { get; set; }
+
+        }
+    
+}
