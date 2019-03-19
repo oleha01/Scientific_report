88e98044a50c422ed1482006feb71c7867d82df9
Олег Бутрий <oleha01@butryy.onmicrosoft.com>
2019-03-19 21:42:20 +0200

    Add-model

---
+++ Scientific_report/Models/AppContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Scientific_report.Models
+{
+    public class AppContext:DbContext
+    {
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Work> Works { get; set; }
+        public DbSet<WorkEnum> WorkEnums { get; set; }
+        public DbSet<Work_User> Work_Users { get; set; }
+        public AppContext(DbContextOptions<AppContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLazyLoadingProxies();
+        }
+       
+    }
+}
+
---
+++ Scientific_report/Models/Teacher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scientific_report.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string SurName { get; set; }
+        public string  Name { get; set; }
+        public string Patronymic { get; set; } //По-Батькові
+        public string Position { get; set; }
+        public string Chair { get; set; }
+        public int Year_of_birth { get; set; }
+        public int Year_of_graduation { get; set; }
+        public string Degree { get; set; }
+        public int year_of_Protection { get; set; }
+        public string Academic_status { get; set; }
+        public int year_of_Pssignment { get; set; }
+    }
+    
+}
