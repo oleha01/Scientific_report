d9fd3710f96fe4a835baac8fb4e93d244dd1e750
romankoltun <rmkoltun3@gmail.com>
2019-04-25 14:05:40 +0300

    Controller Work

---
+++ Scientific_report/Controllers/WorksController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Scientific_report.Models;
+
+namespace Scientific_report.Controllers
+{
+    public class WorksController : Controller
+    {
+        private readonly AppReportContext _context;
+
+        public WorksController(AppReportContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Works
+        public async Task<IActionResult> Index()
+        {
+            var appReportContext = _context.Works.Include(w => w.Type);
+            return View(await appReportContext.ToListAsync());
+        }
+        
+
+        // GET: Works/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var work = await _context.Works
+                .Include(w => w.Type)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (work == null)
+            {
+                return NotFound();
+            }
+
+            return View(work);
+        }
+
+        // GET: Works/Create
+        public IActionResult Create()
+        {
+            ViewData["TypeId"] = new SelectList(_context.WorkEnums, "Id", "Id");
+            return View();
+        }
+
+        // POST: Works/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TypeId,Date,Status")] Work work)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(work);
+                await _context.SaveChangesAsync();
+                Work_User _Use = new Work_User {WorkId=work.Id,UserId =-1 };
+                _context.Work_Users.Add(_Use);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TypeId"] = new SelectList(_context.WorkEnums, "Id", "Id", work.TypeId);
+            return View(work);
+        }
+
+        // GET: Works/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var work = await _context.Works.FindAsync(id);
+            if (work == null)
+            {
+                return NotFound();
+            }
+            ViewData["TypeId"] = new SelectList(_context.WorkEnums, "Id", "Id", work.TypeId);
+            return View(work);
+        }
+
+        // POST: Works/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TypeId,Date,Status")] Work work)
+        {
+            if (id != work.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(work);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WorkExists(work.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TypeId"] = new SelectList(_context.WorkEnums, "Id", "Id", work.TypeId);
+            return View(work);
+        }
+
+        // GET: Works/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var work = await _context.Works
+                .Include(w => w.Type)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (work == null)
+            {
+                return NotFound();
+            }
+
+            return View(work);
+        }
+
+        // POST: Works/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var work = await _context.Works.FindAsync(id);
+            _context.Works.Remove(work);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WorkExists(int id)
+        {
+            return _context.Works.Any(e => e.Id == id);
+        }
+    }
+}
