ca9e2ab54496733accd91cf7d23852a06b29c8c6
Ira Burdeina <iruna.burdeina@gmail.com>
2019-05-08 23:25:12 +0300

    Add AcountController

---
+++ Scientific_report/Controllers/AcountController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Scientific_report.Controllers
+{
+    public class AcountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
